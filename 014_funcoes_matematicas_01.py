e4f805b9c00c16af955fefa08fa02b7ec0e71f18
fabiocaettano <fabio.caettano74@gmail.com>
2024-10-20 07:46:46 -0300

    biblioteca math

--- 014_funcoes_matematicas_01.py
+++ 014_funcoes_matematicas_01.py
@@ -3,12 +3,17 @@ import math
 x = 8
 y = 100
 
-# raiz_quadrada = math.sqrt(x)
-# print(round(raiz_quadrada,4))
+#raiz_quadrada = math.sqrt(x)
+#print(round(raiz_quadrada,4))
+#print(math.floor(raiz_quadrada))
+#print(math.ceil(raiz_quadrada))
 
 # logaritimo = math.log10(y)
 # print(logaritimo)
 
 # print(math.pi)
 
-print(math.factorial(5340))
+#fatorial = len(str(math.factorial(5340)))
+#print(fatorial)
+
+print(pow(x,y))

36cb0c85e18430631f53735c1e514be77a3bb317
fabiocaettano <fabio.caettano74@gmail.com>
2024-10-20 07:30:46 -0300

    biblioteca math

---
+++ 014_funcoes_matematicas_01.py
@@ -0,0 +1,14 @@
+import math
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)
+print(round(raiz_quadrada,4))
+
+logaritimo = math.log10(y)
+print(logaritimo)
+
+print(math.pi)
+
+print(math.factorial(120))
